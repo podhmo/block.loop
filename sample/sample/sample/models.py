1727b6ea81427c38c73a44b73f9856e573524c9d
podhmo <podhmo+altair@beproud.jp>
2013-12-14 22:29:57 +0900

    delete annoying MyModel

--- sample/sample/sample/models.py
+++ sample/sample/sample/models.py
@@ -17,21 +17,8 @@ from zope.sqlalchemy import ZopeTransactionExtension
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
-
-class MyModel(Base):
-    __tablename__ = 'models'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    value = Column(Integer)
-
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
-
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     name = Column(Text)
-
-Index('my_index', MyModel.name, unique=True, mysql_length=255)
 
--- sample/sample/sample/views.py
+++ sample/sample/sample/views.py
@@ -1,12 +1,9 @@
 from pyramid.httpexceptions import HTTPFound
-from pyramid.response import Response
 from pyramid.view import view_config
 from functools import partial
-from sqlalchemy.exc import DBAPIError
 import sqlalchemy as sa
 from .models import (
     DBSession,
-    MyModel,
     User
     )
 
@@ -53,30 +50,3 @@ loop = (view_loop()
         .add(partial(add_form, form=user_form))
 )
 user_create_input = view_config(route_name="user.create", request_method="GET", renderer='input.mako')(ViewFromLoopForLazy(loop, "create_user_input"))
-
-
-
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    try:
-        one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
-    except DBAPIError:
-        return Response(conn_err_msg, content_type='text/plain', status_int=500)
-    return {'one': one, 'project': 'sample'}
-
-conn_err_msg = """\
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
-
-1.  You may need to run the "initialize_sample_db" script
-    to initialize your database tables.  Check your virtual 
-    environment's "bin" directory for this script and try to run it.
-
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
-
-After you fix the problem, please restart the Pyramid application to
-try it again.
-"""
-
